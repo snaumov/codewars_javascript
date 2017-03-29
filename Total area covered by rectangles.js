d18970c3bcf3a315efba545fe5b05a47d6ebf23b
st_naumov <s.s.naumov@gmail.com>
2017-03-29 19:19:17 +0300

    m

--- Total area covered by rectangles.js
+++ Total area covered by rectangles.js
@@ -1,42 +1,52 @@
 function calculate(arrayOfRect) {
-    var outterMostLeft = Infinity;
-    var outterMostBottom = Infinity;
-    var outterMostRight = 0;
-    var outterMostTop = 0;
+    // var outterMostLeft = Infinity;
+    // var outterMostBottom = Infinity;
+    // var outterMostRight = 0;
+    // var outterMostTop = 0;
 
-    var resultingArea = 0;
+    // var resultingArea = 0;
 
     var occupiedSquares = new Set();
 
     for (array of arrayOfRect) {
-        for(var y = array[1]; y < array[3] - 1; y++){
-            if (y > outterMostTop) {
-                outterMostTop = y;
-            }
-            if (y < outterMostBottom) {
-                outterMostBottom = y;
-            }
-            
-            for(var x = array[0]; x < array[2] - 1; x++) {
-                if (x > outterMostRight) {
-                    outterMostRight = x;
-                }
-                if (x < outterMostLeft) {
-                    outterMostLeft = x;
-                }
+        // if(array[0] < outterMostLeft) {
+        //     outterMostLeft = array[0];
+        // }
 
+        // if(array[1] < outterMostBottom) {
+        //     outterMostBottom = array[0];
+        // }
+
+        // if(array[2] > outterMostRight) {
+        //     outterMostRight = array[2];
+        // }
+
+        // if(array[3] > outterMostTop) {
+        //     outterMostTop = array[3]
+        // }
+        for(var y = array[1]; y < array[3]; y++){
+            
+            for(var x = array[0]; x < array[2]; x++) {
                 occupiedSquares.add([x, y, x + 1, y + 1].toString())
             }
         }
     }
 
-    for (var y = outterMostBottom; y < outterMostTop; y++){
-        for (var x = outterMostLeft; x < outterMostRight; x++) {
-            if (!occupiedSquares.has([x, y, x + 1, y + 1].toString())){
-                resultingArea += 1;
-            }
-        }
-    }
-
-    return resultingArea;
+    // for (var y = outterMostBottom; y < outterMostTop; y++){
+    //     for (var x = outterMostLeft; x < outterMostRight; x++) {
+    //         if (!occupiedSquares.has([x, y, x + 1, y + 1].toString())){
+    //             resultingArea += 1;
+    //         }
+    //     }
+    // }
+    
+    return arrayOfRect.length === 0 ? 0 : occupiedSquares.size;
+    //(outterMostRight - outterMostLeft) * (outterMostTop - outterMostBottom) - resultingArea;
 }
+
+console.log(calculate([[ 1, 3, 4, 5 ],
+  [ 2, 1, 4, 7 ],
+  [ 3, 4, 5, 6 ],
+  [ 6, 6, 8, 7 ],
+  [ 5, 3, 8, 4 ],
+  [ 6, 0, 7, 3 ]]));
