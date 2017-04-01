6e9f23932df483ace8f23be78af6145a44b3baff
snaumov <s.s.naumov@gmail.com>
2017-04-01 19:28:24 +0300

    semyon's zadachka

--- Total area covered by rectangles.js
+++ Total area covered by rectangles.js
@@ -1,50 +1,3 @@
-function calculate1(arrayOfRect) {
-    // var outterMostLeft = Infinity;
-    // var outterMostBottom = Infinity;
-    // var outterMostRight = 0;
-    // var outterMostTop = 0;
-
-    // var resultingArea = 0;
-
-    var occupiedSquares = new Set();
-
-    for (array of arrayOfRect) {
-        // if(array[0] < outterMostLeft) {
-        //     outterMostLeft = array[0];
-        // }
-
-        // if(array[1] < outterMostBottom) {
-        //     outterMostBottom = array[0];
-        // }
-
-        // if(array[2] > outterMostRight) {
-        //     outterMostRight = array[2];
-        // }
-
-        // if(array[3] > outterMostTop) {
-        //     outterMostTop = array[3]
-        // }
-        for(var y = array[1]; y < array[3]; y++){
-            
-            for(var x = array[0]; x < array[2]; x++) {
-                occupiedSquares.add([x, y, x + 1, y + 1].toString())
-            }
-        }
-    }
-
-    // for (var y = outterMostBottom; y < outterMostTop; y++){
-    //     for (var x = outterMostLeft; x < outterMostRight; x++) {
-    //         if (!occupiedSquares.has([x, y, x + 1, y + 1].toString())){
-    //             resultingArea += 1;
-    //         }
-    //     }
-    // }
-    
-    return arrayOfRect.length === 0 ? 0 : occupiedSquares.size;
-    //(outterMostRight - outterMostLeft) * (outterMostTop - outterMostBottom) - resultingArea;
-}
-
-
 function multiDimArraySort(arr1, arr2) {
     for(i = 0; i < 4; i++){
         if(arr1[i] < arr2[i]) {
@@ -58,7 +11,7 @@ function multiDimArraySort(arr1, arr2) {
 }
 
 function currentRectsYSort(arr1, arr2) {
-    return arr1[0] - arr2[0] || arr1[3] - arr2[3];
+    return arr1[1] - arr2[1] || arr1[3] - arr2[3];
 }
 
 function calculate(arrayOfRect) {
@@ -81,13 +34,9 @@ function calculate(arrayOfRect) {
         }
         var nextX = (() => {
             for (arr of sortedArrOfRect.slice(1)) {
-                // if(arr[0] <= currentX) {
-                //     currentRects.push(arr);
-                // }
                 if (arr[0] > currentX && arr[0] < currentRects[0][2]) {
                     return arr[0];
                 } else if (arr[0] > currentX) {
-                    //sortedArrOfRect.shift();
                     return currentRects[0][2];
                 } else if(arr[2] > currentX && arr[2] < sortedArrOfRect[0][2]) {
                     return arr[2];
@@ -119,47 +68,54 @@ function calculate(arrayOfRect) {
                         return arr[1];
                     } else if (arr[1] > currentY) {
                         var nextY = currentRects[0][3];
-                        currentRects.shift();
                         return nextY;
-                    } else if (arr[3] > currentY && arr[1] !== currentY) {
-                        var index = currentRects.map(arr => arr.toString()).indexOf(arr.toString());
-                        currentRects.splice(index, 1)
+                    } else if (arr[3] < currentRects[0][3]) {
                         return arr[3];
                     }
                 }
                 var nextY = currentRects[0][3]
-                currentRects.shift();
                 return nextY;
             })();
 
             resultingSquare += (nextX - currentX) * (nextY - currentY);
 
+            var tempCurrentRects = [];
+            var i = 0;
+            for (arr of currentRects) {
+                if(arr[1] > nextY) {
+                    break;
+                }
+                if(arr[3] > nextY) {
+                    tempCurrentRects.push(arr);
+                }
+                i++
+            }
+            currentRects = tempCurrentRects.concat(currentRects.slice(i));
+            
             currentY = nextY;
         }
         
-        currentX = nextX;
 
+        var tempSortedArrOfRect = [];
         var i = 0;
-        
-        for(arr of sortedArrOfRect) {
-            if(arr[2] > currentX){
+        for (arr of sortedArrOfRect) {
+            if(arr[0] > nextX) {
                 break;
             }
-            if(arr[2] <= currentX){
-                sortedArrOfRect.splice(i, 1);
+            if(arr[2] > nextX) {
+                tempSortedArrOfRect.push(arr);
             }
             i++;
         }
-        //sortedArrOfRect = sortedArrOfRect.slice(i);
+        sortedArrOfRect = tempSortedArrOfRect.concat(sortedArrOfRect.slice(i));
+
+        currentX = nextX;
     }
     
     return resultingSquare;
 }
 
 
-console.log(calculate([[ 1, 3, 4, 5 ],
-  [ 2, 1, 4, 7 ],
-  [ 3, 4, 5, 6 ],
-  [ 6, 6, 8, 7 ],
-  [ 5, 3, 8, 4 ],
-  [ 6, 0, 7, 3 ]]));
+console.log(calculate([
+[3,3,8,5], [6,3,8,9],[11,6,14,12]
+]));
